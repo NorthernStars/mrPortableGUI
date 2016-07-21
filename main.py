407f2c04470dc4d8b8020c81ff7d23ccef780934
Hannes Eilers <mail@hanneseilers.de>
2016-07-21 16:35:36 +0200

    Added layout file and added example layout

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ os.environ['KIVY_BCM_DISPMANX_ID'] = '4'
 
 
 import kivy
-kivy.require('1.9.2') # replace with your current kivy version !
+kivy.require('1.9.0') # replace with your current kivy version !
 
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+
+class RootLayout(BoxLayout):
+	pass
 
 
 class MRPortableGui(App):
 
     def build(self):
-        return Label(text='mrPortableGUI')
+        return RootLayout()
 
 
 if __name__ == '__main__':
