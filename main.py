91e4881fa3ef50934e305a4e76f8bde97c9617a2
Hannes Eilers <mail@hanneseilers.de>
2016-07-25 17:14:46 +0200

    Updated GUI. Addded images.

--- main.py
+++ main.py
@@ -6,17 +6,33 @@ import kivy
 kivy.require('1.9.0') # replace with your current kivy version !
 
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-
-class RootLayout(BoxLayout):
-	pass
-
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.button import Button
+from kivy.uix.image import Image
 
 class MRPortableGui(App):
 
     def build(self):
-        return RootLayout()
+    	self.screenManager = ScreenManager()
+    	self.screenManager.add_widget( MainScreen(name="MainScreen") )
+    	self.screenManager.add_widget( PowerOffScreen(name="PowerOffScreen") )
+        return self.screenManager;
+        
+class MainScreen(Screen):
+	pass
+	
+class PowerOffScreen(Screen):
+	pass
 
+class ImageButton(Button):
+
+	source = ""
+
+	def __init__(self, **kwargs):
+		super(ImageButton, self).__init__(**kwargs)
+		self.background_color = (0,0,0,0)
+	pass
+	
 
 if __name__ == '__main__':
     MRPortableGui().run()
